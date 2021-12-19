0d223d20500d82e8482a2165a3bee0a4743a1928
aremizov <aremizov@aori.ru>
2021-12-19 14:52:09 +0300

    Lesson 2 add Person

---
+++ Lesson2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String sex = "Man";
+    String name = "Andrei";
+    float height = 2.05f;
+    float weight = 110.1f;
+    int age = 29;
+
+    void walk() {
+        System.out.println();
+    }
+
+    boolean sitDown() {
+        return true;
+    }
+
+    void run() {
+        System.out.println();
+    }
+
+    String say() {
+        return "Say";
+    }
+
+    void learn() {
+        System.out.println("Java");
+    }
+}
